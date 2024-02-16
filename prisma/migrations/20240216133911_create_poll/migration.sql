450f3751392e9c167c37e25c34c2f668f646b815
WalanyCosta <walanybnegro@gmail.com>
2024-02-16 15:06:39 +0100

    primeira aula-01

---
+++ prisma/migrations/20240216133911_create_poll/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Poll" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Poll_pkey" PRIMARY KEY ("id")
+);
